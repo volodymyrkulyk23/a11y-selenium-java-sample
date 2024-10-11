ff956e881788dc5574f0cae9fc6400a94647be60
Volodymyr Kulyk <volodymyr.kulyk@userway.org>
2024-10-11 13:54:51 +0300

    main branch - 1st scan test

---
+++ c11y/userway.config.ts
@@ -0,0 +1,15 @@
+import { Config } from '@userway/cicd-core';
+
+export default {
+  token: process.env.USERWAY_TOKEN,
+  organization: 'volodymyr-kulyk-userway-org-igpgx',
+  project: 'a11y-selenium-java-sample',
+  reportPaths: ["../uw-a11y-reports"],
+  server: "https://api.qa.userway.dev",
+  override: {
+    'main': {
+      retention: 'long',
+      scope: 'overall',
+  },
+}
+} satisfies Config;
